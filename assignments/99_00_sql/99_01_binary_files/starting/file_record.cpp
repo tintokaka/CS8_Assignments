7c2bc292614857267e3d3e2b8ba364415e4c74c3
Tan Bao Tin Nguyen <93857461+tintokaka@users.noreply.github.com>
2023-04-29 22:05:50 -0700

    Update file_record.cpp

--- assignments/99_00_sql/99_01_binary_files/starting/file_record.cpp
+++ assignments/99_00_sql/99_01_binary_files/starting/file_record.cpp
@@ -2,33 +2,30 @@
 
 long FileRecord::write(fstream &outs)
 {
-  // Take the Record r and write it into file f and return the record number of this Record
-  // r.write(f); calls this function
-
-  // Write to the end of the file
-  outs.seekg(0, outs.end);
-  // Retrieves the current position of the file pointer
-  long pos = outs.tellp();
-  // Insert the size of record items into file
-  outs.write(_records, sizeof(_records));
-  // Return the record number
-  return pos / sizeof(_records);
+       outs.seekg(0, outs.end);
+        long pos = outs.tellp();
+        for (char* record : _records) {
+            outs.write(record, MAX+1);
+        }
+        return pos / (_records.size() * (MAX+1));
 }
 
 long FileRecord::read(fstream &ins, long recno)
-{
-  // Save sixth record in file to Record r and returns the number of bytes read
-  // r.read(f, 6); calls this function
-
-  // Calculate the position of the record in the file
-  long pos = recno * sizeof(_records);
-  // Set the file pointer to the pos starting from the beginning of the file
-  ins.seekg(pos, ios_base::beg);
-  // Read the record from the file
-  ins.read(_records, sizeof(_records));
-  // Set the end of cstring with null
-  // For example: _records[] => [zero'\0' trash ... trash]
-  _records[ins.gcount()] = '\0';
-  // Return the number of bytes read
-  return ins.gcount();
+{   
+     long bytesRead = 0;
+    for (char* record : _records) {
+        if (!ins.read(record, MAX+1)) {  // If read fails
+            if (ins.eof()) {
+                break;  // Stop reading at the end of file
+            } else {
+                // Error handling code here
+                return -1;
+            }
+        }
+        bytesRead += ins.gcount();
+        if (*record == '\0') {
+            break;
+        }
+    }
+    return bytesRead;
 }
